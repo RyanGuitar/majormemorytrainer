fb50e29b05c4e584939fdb8f9ad198ea0407f57c
RyanGuitar <ryanelsguitar@gmail.com>
2021-10-21 15:52:07 +0200

    added service worker

--- App.js
+++ App.js
@@ -1,1 +1,4 @@
-import{elAdd,addClick,importPage}from"./scripts/helpers.js";const landingHTML='<div id="Page">\n    <div id="Box">\n      <div id="Title">Major Memory Trainer</div>\n      <br>\n      <div id="SubTitle">click to start learning</div>\n    </div>\n  </div>';elAdd("AppContent",landingHTML),addClick("Box",()=>importPage("menu"));
+import { elAdd, addClick, importPage } from "./scripts/helpers.js";
+const landingHTML =
+  '<div id="Page">\n    <div id="Box">\n      <div id="Title">Major Memory Trainer</div>\n      <br>\n      <div id="SubTitle">click to start learning</div>\n    </div>\n  </div>';
+elAdd("AppContent", landingHTML), addClick("Box", () => importPage("menu"));
